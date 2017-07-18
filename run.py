d7df188e829c1f10733e06c0c679edca256956ef
arashout <arash.out@gmail.com>
2017-07-18 02:02:02 -0700

    Adding command line arguemnts

--- run.py
+++ run.py
@@ -1,18 +1,30 @@
+import argparse
+from enum import Enum
+
 from Bot.AngelBot import AngelBot
 from Bot.Indeed.IndeedBot import IndeedRobot
 from userconfig import UserConfig
 
-if __name__ == "__main__":
-    INDEED = True
-    if INDEED:
-        bot = IndeedRobot(UserConfig())
+class JobBot(Enum):
+    IndeedBot = 1
+    AngelBot = 2
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run JobBot')
+    parser.add_argument('option', type=int, help="Pick JobBot, by typing it's option number")
+
+    unformatted_display_query = "Bot being initialized with this query:\n\n {0}"
+
+    args = parser.parse_args()
+    if args.option == JobBot.IndeedBot:
         q1 = '((intern OR co-op) AND ' \
              '(software OR develop OR engineer OR mechanical OR mechatronics ' \
              'OR programming OR android OR ios OR technical OR qa OR testing OR automation)) ' \
              '-"marketing" -"human resources" -"hr" -unpaid -volunteer -"labor" -"labour" -secretary ' \
-             '-receptionist -"assistant" -clerk -instructor -coordinator -tutor -cook -operator -manager -accountant' \
-             '-senior -director -film -"social worker" -teacher -designer -psychologist -"architect'
+             '-receptionist -"assistant" -clerk -instructor -coordinator -tutor -cook -operator -manager -accountant ' \
+             '-senior -director -film -"social worker" -teacher -designer -psychologist -"architect" ' \
+             '-optician -optician'
+
         params = {
             'q': q1,
             'limit': '25',
@@ -23,13 +35,16 @@ if __name__ == "__main__":
             'userip': "1.2.3.4",
             'useragent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
         }
-        #bot.search_with_api(params=params)
+
+        print(unformatted_display_query.format(q1))
+
+        bot = IndeedRobot(UserConfig())
+        bot.search_with_api(params=params)
         bot.login()
         bot.apply_jobs()
         bot.shut_down()
-    else:
-        bot = AngelBot(UserConfig())
-        bot.login()
+
+    elif args.option == JobBot.AngelBot:
         query_parameters = {"types": "internship",
                             "roles": [
                                 "Software Engineer"
@@ -37,7 +52,16 @@ if __name__ == "__main__":
                             "last_active" : "30",
                             "excluded_keywords": ["unpaid"]
                             }
+        print(unformatted_display_query.format(query_parameters))
+
+        bot = AngelBot(UserConfig())
+        bot.login()
         #bot.gather(query_parameters)
         bot.apply()
         bot.shut_down()
+
+    else:
+        print('Pick one of these options:')
+        for bot in JobBot:
+            print('{0} : {1}'.format(bot.value, bot.name))
 
